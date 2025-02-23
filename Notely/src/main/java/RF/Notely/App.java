43dab7ee461a242ab47e98b0485bf85b55478612
Sossics <michelerusso267@gmail.com>
2025-02-23 21:11:30 +0100

    Inizializzazione progetto Maven

---
+++ Notely/src/main/java/RF/Notely/App.java
@@ -0,0 +1,13 @@
+package RF.Notely;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
