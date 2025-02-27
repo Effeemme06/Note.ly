122b6bccf395614bbee9d6bafc39cd258490866e
Effeemme06 <effeemmeco6@gmail.com>
2025-02-27 11:31:15 +0100

    Create Gestion_Notepad.java

---
+++ Notely/src/main/java/RF/Notely/app/model/Gestion_Notepad.java
@@ -0,0 +1,50 @@
+package RF.Notely.app.model;
+
+import java.util.Scanner;
+
+public class Gestion_Notepad {
+	
+	private static String MENU_NOTEPAD = "1.\tView Note\n2.\tCreate Note\n3.\tDelete Note\n4.\tRename Notepad\n0.\tback\n>>";
+    private Scanner user_input;
+	
+	public void GenerateMenu(int id_notepad) {
+		
+		
+		boolean exit = false;
+		int selection = 0;
+		
+		while(exit == false) {
+			
+			try {
+				
+				System.out.println(MENU_NOTEPAD);
+	    		selection = user_input.nextInt();
+	    		switch (selection) {
+					case 0:
+						exit = true;
+						break;
+					case 4:
+						//rinomina blocco
+						break;
+					case 3:
+							//lista note per eliminare
+						break;
+					case 2:
+							//crea nota
+						break;
+					case 1:
+							//lista note selezionabili--> classe per visualizzare note / rinominare
+						break;
+					default:
+						break;
+				}
+    		} catch (Exception e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+			
+		}
+		
+	}
+	
+}
