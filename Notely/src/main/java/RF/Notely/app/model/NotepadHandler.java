d7abbc8e8a444181cb69d0e4bb6e8a05f85fd353
Sossics <michelerusso267@gmail.com>
2025-02-28 11:52:36 +0100

    gyou8gho9gih2ò8ry29r7yr7y89285989034503490534'053

--- Notely/src/main/java/RF/Notely/app/model/NotepadHandler.java
+++ Notely/src/main/java/RF/Notely/app/model/NotepadHandler.java
@@ -8,25 +8,20 @@ public class NotepadHandler {
 	
 	private static String MENU_NOTEPAD = "\n1.\tSelect Note\n2.\tCreate Note\n3.\tDelete Note\n4.\tRename Notepad\n0.\tback\n>>";
     private Scanner user_input;
+    private Store store;
     private WebServiceClient WSC;
 	
 	public void GenerateMenu(int id_notepad, Store ntp) {
 		
+		this.store = ntp;
 		WSC = Notely.getInstance().getWSC();
 		
 		user_input = new Scanner(System.in);
 		
 		boolean exit = true;
 		NotePad selected = null;
-		
-		try {
-			ntp = WSC.getNotepads();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
-		for (NotePad np : ntp.getNotePads()) {
+		for (NotePad np : store.getNotePads()) {
 			if(np.getID() == id_notepad) {
 				exit = false;
 				selected = np;
@@ -61,7 +56,7 @@ public class NotepadHandler {
 						
 						// TODO
 						//rinomina blocco
-						
+						updateStore();
 						break;
 					case 3:
 						//lista note per eliminare
@@ -72,6 +67,7 @@ public class NotepadHandler {
 						System.out.println("\nDelete note:\n>>");
 						selection = user_input.nextInt();
 						boolean success = WSC.deleteNote(selection);
+						updateStore();
 						break;
 					case 2:
 						//crea nota
@@ -80,6 +76,7 @@ public class NotepadHandler {
 						System.out.print("Write the note > ");
 						String body = user_input.nextLine();
 						WSC.createNote(title_note, body, id_notepad);
+						updateStore();
 						break;
 					case 1:
 						System.out.println("Note:");
@@ -102,6 +99,10 @@ public class NotepadHandler {
 		
 		System.out.println("\nExiting...\n");
 
+	}
+
+	private void updateStore() throws Exception {
+		this.store = WSC.getNotepads();
 	}
 	
 }
